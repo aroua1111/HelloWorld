c2a6053b11653081de8b5c46c089b6da7bb9b8a5
Aroua Gitlab <arouabelghith@gmail.com>
2024-02-16 22:25:55 +0100

    add the Project

---
+++ src/test/java/demo/HelloWorld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
